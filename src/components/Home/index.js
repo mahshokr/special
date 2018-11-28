75a94008f21ffc9fc343ea2ccdb6d4bc752787d4
mahmoud shokr <mahmoudshokr44@gmail.com>
2018-11-28 17:11:27 +0200

    navigation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,63 @@
-import React, { Component } from 'react'
-import { Text, View , TextInput } from 'react-native'
-
+import React, { Component } from "react";
+import { Text, View, TextInput, Button ,Alert} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Form,
+  Item,
+  Input,
+  Label
+} from "native-base";
+import styles from "./styles";
 export default class Home extends Component {
+
+
+    state = {username : '' , password : ''}
+
+    CheckLogin =()=> {
+    
+        const {username , password} = this.state
+        //console.warn('user name : ' , password)
+      
+        if(username == 'admin' && password == 'admin'){
+          //  console.warn('user name : ' , username)
+            // redirect to Dashboard
+          this.props.navigation.navigate('dashboard')
+        }else{
+          //alert something is wrong
+          Alert.alert('Error' , 'username / password is not correct') ;
+        }
+    }
+
   render() {
+    const { heading, input, button, parentt } = styles;
+
     return (
-      <View>
-        <Text> Log Into Our application </Text>
-        <TextInput />
-        <TextInput />
-      </View>
-    )
+      <Container>
+        <Content>
+          <View style={parentt}>
+            <Text style={heading}>Login to our project</Text>
+            <Form>
+              <Item floatingLabel>
+                <Label>Username</Label>
+                <Input onChangeText={text => {this.setState({username : text})}}/>
+              </Item>
+              <Item floatingLabel last>
+                <Label>Password</Label>
+                <Input secureTextEntry={true} onChangeText={text => {this.setState({password : text})}}/>
+              </Item>
+              <Button
+                title="Login"
+                onPress={() => {
+                    this.CheckLogin()
+                  //this.props.navigation.navigate("dashboard");
+                }}
+              />
+            </Form>
+          </View>
+        </Content>
+      </Container>
+    );
   }
 }
---
+++ src/components/Home/styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native'
+export default StyleSheet.create({
+    heading : {
+        fontSize : 25,
+        textAlign : 'center' , 
+        color : 'tomato'
+
+    } ,
+    input : {
+        
+        marginLeft : 20 ,
+        marginRight : 20 ,
+      
+    } , 
+    button : {
+      
+    } , 
+    parentt : {
+        flex : 1,
+        flexDirection : 'column',
+        justifyContent : 'center' ,
+        marginTop : 150
+    }
+})
